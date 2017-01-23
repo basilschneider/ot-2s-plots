4ac5697f48b7bd1504a09ebe11ee5fcb6bf8fd56
Basil Schneider <basil.schneider@cern.ch>
2017-01-23 09:35:30 -0600

    Initial commit

---
+++ ot-2s-plots.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+""" Extract all plots from Middleware scan. """
+
+from plots import plots
+
+if __name__ == "__main__":
+
+    plots = plots('Commissioning.root', 'test01')
+    plots.getPlots()
---
+++ plots.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+""" Extract all plots from Middleware scan. """
+
+from os import makedirs, chdir, getcwd, path
+from ROOT import TFile, TCanvas
+
+class plots(object):
+
+    """ Extract all plots from Middleware scan. """
+
+    def __init__(self, inputfile, outputfolder=''):
+
+        """ Initialize object variables. """
+
+        self._rootfile = TFile(inputfile)
+        self._directory = outputfolder
+        self._canvas = TCanvas()
+        makedirs(outputfolder)
+
+    def getPlots(self):
+
+        """ Get all the plots. """
+
+        self._loopKeys()
+
+    def _loopKeys(self, subdir=None):
+
+        """ Loop over all keys in a given subfolder. """
+
+        # If subdir is defined, loop over keys in folder, otherwise loop over
+        # keys in rootfile
+        if subdir:
+
+            obj = self._rootfile.Get(subdir)
+        else:
+            obj = self._rootfile
+
+        # Loop over all keys
+        for key in obj.GetListOfKeys():
+
+            if subdir:
+                kname = '{}/{}'.format(subdir, key.GetName())
+            else:
+                kname = key.GetName()
+            # If key is a folder, go into that folder and loop over all keys
+            if key.IsFolder():
+                self._loopKeys(kname)
+                continue
+
+            # Clear all objects from canvas
+            self._canvas.Clear()
+
+            # Get histogram and draw it
+            histo = self._rootfile.Get(kname)
+            histo.Draw()
+
+            # Save it as pdf
+            self._save(kname)
+
+    def _save(self, name):
+
+        """ Save histogram as *.pdf. """
+
+        # Go into directory if it is defined
+        if self._directory:
+            cwd = getcwd()
+
+            # Create directory on filesystem
+            if not path.exists(self._directory):
+                makedirs(self._directory)
+            chdir(self._directory)
+
+        # If canvas is to be stored in a subdirectory, create it
+        if '/' in name:
+            subdir = name[:name.find('/')]
+            if not path.exists(subdir):
+                makedirs(subdir)
+
+        self._canvas.SaveAs('{}.pdf'.format(name))
+
+        # Go back to original working directories
+        if self._directory:
+            chdir(cwd)
